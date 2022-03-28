788bbabbe37a6bbc6c0d89ab639425ba75e39929
bruno-ramosb <69005785+bruno-ramosb@users.noreply.github.com>
2022-03-28 14:51:24 -0400

    Adiciona pontuacao
    Adicionado pontuacao para o jogo de advinhacao, onde pontos inicial = 1000 e para calculo dos pontos é a diferença entre numero secreto e chutes

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,6 +6,7 @@ print("**********************************")
 
 numero_secreto= random.randrange(1,101) # 1 to 100 > +1 to generate till 100
 total_tentativas=0
+pontos=1000
 
 print("Qual o nível de dificuldade ?")
 print("(1) Fácil (2) Médio (3) Difícil")
@@ -25,20 +26,23 @@ for rodada in range(1,total_tentativas+1):
     print("Você digitou ",chute_str)
     chute=int(chute_str)
 
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número de 1 a 100!")
+        continue
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if(chute<1 or chute >100):
-        print("Você deve digitar um número de 1 a 100!")
-        continue
-    elif(acertou):
-        print("Você acertou !")
+    if(acertou):
+        print("Você acertou e fez {} pontos !".format(pontos))
         break
-    elif(maior):
-        print("Chute realizado maior que o número secreto")
-    elif(menor):
-        print("Chute realizado menor que o número secreto")
-
+    else:
+        if(maior):
+            print("Chute realizado maior que o número secreto")
+        elif(menor):
+            print("Chute realizado menor que o número secreto")
+            pontos_perdidos = abs(numero_secreto - chute) #40 - 30 > perde 10 , 60-80 = -20 > para arrendodar => abs
+            pontos = pontos - pontos_perdidos
 
 
