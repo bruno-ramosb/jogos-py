3781a60e4b353131deb46f0fb6c901db3e5cabcd
bruno-ramosb <69005785+bruno-ramosb@users.noreply.github.com>
2022-03-28 17:04:49 -0400

    Adiciona jogo da forca

--- forca.py
+++ forca.py
@@ -6,5 +6,41 @@ def jogar():
     print("Bem vindo ao jogo da forca")
     print("**********************************")
 
+    palavra_secreta="banana".upper()
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    tentativas = 0
+    enforcou= False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra ?").upper().strip()
+
+        if(chute in palavra_secreta):
+            index=0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    print("Encontrado a letra {} na posição {}".format(letra,index))
+                    letras_acertadas[index]= letra
+
+                index += 1
+        else:
+            tentativas += 1
+
+        enforcou = tentativas == 6
+        acertou  = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu !")
+    print("Fim de jogo")
+
+
+    print("Jogando ....")
+
+
 if(__name__ == "__main__"):
     jogar()
